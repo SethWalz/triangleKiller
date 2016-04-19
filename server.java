fabe91a196563c96f04e010ad7dcaaff1378a1e2
Swalz0516 <Swalz0516@gmail.com>
2016-04-19 10:23:23 -0500

    Server

--- server.java
+++ server.java
@@ -6,3 +6,52 @@ Seth Walz
 Adv_Java Game | Triangle Killer
 
 */
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class server {
+
+	private static final int PORT = 8000;
+    private static HashSet<Double> players = new HashSet<>();
+
+    public static void main(String[] args) {
+        System.out.println("Server online.");
+
+        try (ServerSocket chatServer = new ServerSocket(PORT)) {
+            while (true) {
+                Socket socket = chatServer.accept();
+                new Handler(socket).start();
+            }
+        } catch (IOException ioe) {}
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+
+
+            } catch (IOException e) {
+
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {}
+            }
+        }
+    }
+
+
+}
